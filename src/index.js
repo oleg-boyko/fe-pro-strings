26946090e446daed2605cf996b8f0230e459032e
oleg-boyko <97238155+oleg-boyko@users.noreply.github.com>
2022-06-16 20:30:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ export const replaceZAndVFromString = (string) => {  //не мог почему-
 export const changeWord = (string, word, newWord) => {
     let result = 'string', i = 0, wordSubLen = 'word'.length;
     while (wordSubLen) {
-      i = result.toLowerCase().indexOf(word, i);
+      i = result.toLowerCase().indexOf('word', i);
       if (i === -1) {
       break;
       };
